e981b0d76c30fe79181da37a924e05d7dd549f6c
Davids <davidsgabalins@gmail.com>
2018-04-08 02:14:45 +0300

    Update sum2.php

--- sum2.php
+++ sum2.php
@@ -1,26 +1,65 @@
 <?php
+/*
+This is basically a re-write of 
+https://www.geeksforgeeks.org/sum-two-large-numbers/
+but in PHP and with file r/w
+*/
 function sum2() {
-	ini_set("auto_detect_line_endings", true); // for end lines with \r
+	ini_set("auto_detect_line_endings", true);
 
-	$input = fopen('input_file.in', 'r'); // open input file in readmode
-	$output = fopen('output_file.out', 'w'); // open output file in writemode
+	$input = fopen('input_file.in', 'r');
+	$output = fopen('output_file.out', 'w');
 
-	if (!$input || !$output) { // if either don't open, return
+	if (!$input || !$output) {
 		return;
 	}
 
-	while (!feof($input)) { // while hasn't reached the end of file
-		$lines[] = fgets($input, 9999); // push lines to array
+	$lines = [];
+	while (!feof($input)) {
+		$lines[] = fgets($input, 9999);
 	}
 
-	fclose($input); // close input file
+	fclose($input);
 
-	if (isset($lines) && !empty($lines)) { // if there are lines
-		$sum = intval($lines[0]) + intval($lines[1]); // count the integer values of first two
-		fwrite($output, $sum); // output the sum to the output file
-		fclose($output); // close the output file
+	if (isset($lines) && !empty($lines)) {
+		// longer string has to be second
+		if (strlen($lines[0]) > strlen($lines[1])) {
+			$str1 = $lines[1];
+			$str2 = $lines[0];
+		} else {
+			$str1 = $lines[0];
+			$str2 = $lines[1];
+		}
+
+		$result = "";
+		$str1 = strrev(str_replace(' ', '', $str1)); // reverse and remove spaces
+		$str2 = strrev(str_replace(' ', '', $str2));
+
+		$count1 = strlen($str1);
+		$count2 = strlen($str2);
+		
+		$carry = 0; // digit to carry over
+
+		for ($i = 1; $i < $count1; $i++) { // fgets() returns a string+\n, so have to start at 1
+			$sum = (intval($str1[$i]) + intval($str2[$i]) + $carry);
+			$result .= $sum%10;
+			$carry = intval($sum/10);
+		}
+
+		for ($i = $count1; $i < $count2; $i++) {
+			$sum = (intval($str2[$i]) + $carry);
+			$result .= $sum%10;
+			$carry = intval($sum/10);
+		}
+
+		if ($carry) {
+			$result .= $carry;
+		}
+
+		$result = strrev($result);
+		fwrite($output, $result);
+		fclose($output);
 	}
-
 	return;
 }
 
